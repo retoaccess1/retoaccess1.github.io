128d9035e5f8a558977adba54999a0645365381e
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-07-16 15:19:55 +0200

    feat: add new ui

---
+++ src/data/downloads.ts
@@ -0,0 +1,22 @@
+export const downloads = [
+	{
+		id: 1,
+		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.8/HavenoInstaller-windows-latest.zip",
+		title: "Download for Windows",
+	},
+	{
+		id: 2,
+		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.8/haveno_1.0.8-1_amd64-deb.zip",
+		title: "Download for Linux (.deb)",
+	},
+	{
+		id: 3,
+		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.8/haveno-1.0.8-1_x86_64-rpm.zip",
+		title: "Download for Linux (.rpm)",
+	},
+	{
+		id: 4,
+		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.8/HavenoInstaller-macos-13.zip",
+		title: "Download for Mac",
+	},
+];
