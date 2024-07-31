48b4ac8ccb0c47a1a7c72d7e22c80a990d851ad0
bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2024-07-31 21:31:41 +0200

    feat: make download links dynamic

--- src/data/downloads.ts
+++ src/data/downloads.ts
@@ -1,27 +1,27 @@
 export const downloads = [
 	{
 		id: 1,
-		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.8/HavenoInstaller-windows-latest.zip",
+		link: "https://github.com/retoaccess1/haveno-reto/releases/latest/download/HavenoInstaller-windows-latest.zip",
 		title: "Download for Windows",
 	},
 	{
 		id: 2,
-		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.8/haveno_1.0.8-1_amd64-deb.zip",
+		link: "https://github.com/retoaccess1/haveno-reto/releases/latest/download/haveno_amd64-deb.zip",
 		title: "Download for Linux (.deb)",
 	},
 	{
 		id: 3,
-		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.8/haveno-1.0.8-1_x86_64-rpm.zip",
+		link: "https://github.com/retoaccess1/haveno-reto/releases/latest/download/haveno_x86_64-rpm.zip",
 		title: "Download for Linux (.rpm)",
 	},
 	{
 		id: 4,
-		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.8/HavenoInstaller-macos-13.zip",
+		link: "https://github.com/retoaccess1/haveno-reto/releases/latest/download/HavenoInstaller-macos-13.zip",
 		title: "Download for Mac",
 	},
 	{
 		download: "reto-public",
-		id: 4,
+		id: 5,
 		link: "/reto_public.asc",
 		title: "Download Public key",
 	},
