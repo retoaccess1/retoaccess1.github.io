c686f19c8a4a8ecc8a5151d34453db2cf6de090c
retoaccess1 <retoaccess@proton.me>
2024-08-02 08:55:56 +0200

    Revert "Website UI Redesign "

--- src/data/features.ts
+++
@@ -1,6 +0,0 @@
-export const features = [
-	{ id: 1, title: "The first fully functional Haveno instance on mainnet" },
-	{ id: 2, title: "A low arbitration fee network - 0% taker, 0.6% maker" },
-	{ id: 3, title: "A network with zero general fees - fees go exclusively to arbitrators" },
-	{ id: 4, title: "Essentially Bisq 1 adapted for Monero" },
-];
