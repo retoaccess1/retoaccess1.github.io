9eed444b8c15851217a60bf2919e5fd7c1ee88cb
retoaccess1 <retoaccess@proton.me>
2024-08-11 18:02:37 +0200

    Update downloads.ts

--- src/data/downloads.ts
+++ src/data/downloads.ts
@@ -1,22 +1,22 @@
 export const downloads = [
 	{
 		id: 1,
-		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.9/HavenoInstaller-windows-latest.zip",
+		link: "https://github.com/retoaccess1/haveno-reto/releases/latest/download/HavenoInstaller-windows-latest.zip",
 		title: "Download for Windows",
 	},
 	{
 		id: 2,
-		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.9/haveno_1.0.9-1_amd64.deb",
+		link: "https://github.com/retoaccess1/haveno-reto/releases/latest/download/haveno_amd64_deb-latest.zip",
 		title: "Download for Linux (.deb)",
 	},
 	{
 		id: 3,
-		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.9/haveno-1.0.9-1.x86_64.rpm",
+		link: "https://github.com/retoaccess1/haveno-reto/releases/latest/download/haveno_x86_64_rpm-latest.zip",
 		title: "Download for Linux (.rpm)",
 	},
 	{
 		id: 4,
-		link: "https://github.com/retoaccess1/haveno-reto/releases/download/v1.0.9/HavenoInstaller-macos-13.zip",
+		link: "https://github.com/retoaccess1/haveno-reto/releases/latest/download/HavenoInstaller-macos-13.zip",
 		title: "Download for Mac",
 	},
 	{
