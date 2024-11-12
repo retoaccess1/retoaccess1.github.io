545627fd731605e9a0c0d3dbc8e443d57b7a8b61
retoaccess1 <retoaccess@proton.me>
2024-11-12 18:57:05 +0000

    Update features.ts

--- src/data/features.ts
+++ src/data/features.ts
@@ -1,6 +1,6 @@
 export const features = [
 	{ id: 1, title: "The first fully functional Haveno instance on mainnet" },
 	{ id: 2, title: "ZERO arbitration fees - 0% taker, 0% maker" },
-	{ id: 3, title: "A network with zero general fees - fees go exclusively to arbitrators" },
+	{ id: 3, title: "Growing liquidity & volume" },
 	{ id: 4, title: "Essentially Bisq 1 adapted for Monero" },
 ];
