3e729c4588da50f16438b380c44dc837a0771bb8
retoaccess1 <retoaccess@proton.me>
2024-11-29 19:06:08 +0100

    first

--- src/data/features.ts
+++
@@ -1,6 +0,0 @@
-export const features = [
-	{ id: 1, title: "The first fully functional Haveno instance on mainnet" },
-	{ id: 2, title: "ZERO arbitration fees - 0% taker, 0% maker" },
-	{ id: 3, title: "Growing liquidity & volume" },
-	{ id: 4, title: "Essentially Bisq 1 adapted for Monero" },
-];
